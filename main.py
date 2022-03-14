95c335d9b52ee64e83f61a58aa36ebfcfbdba944
Ankur Anand <86293067+anandankur2816@users.noreply.github.com>
2022-03-15 00:39:06 +0530

    added colors to buttons

--- main.py
+++ main.py
@@ -93,9 +93,9 @@ password_entry = Entry(width=26)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command=gen_password)
+generate_password_button = Button(text="Generate Password", command=gen_password, bg=PINK)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=36, command=save, bg=GREEN)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
